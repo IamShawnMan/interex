eaaffe9905cf4a6e6a0bc9737eb5a18bac1de998
Shodiyor Ergashev <mybusinesskebap@gmail.com>
2022-10-05 16:15:22 +0500

    test2

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Interex</h1>
-      <h1>Web app</h1>
+      <h1>Web app</h1> <span>dhgdhdfh</span>
+      <h2>dsgdfgdfg</h2>
     </div>
   );
 }
