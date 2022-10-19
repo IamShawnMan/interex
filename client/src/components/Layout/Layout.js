84203085fdc9aa2b9421b35b7e6f3d0ddfc26209
Umurzoq <omonov0710@mail.com>
2022-10-19 21:56:52 +0500

    Layout component Completed

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./Sidebar/Sidebar";
 import Navbar from "./Navbar/Navbar";
 
+import styles from "./Layout.module.css";
+
 function Layout(props) {
+  const [burger, setBurger] = useState(false);
+
+  const sidebarActiveHandle = () => {
+    setBurger(!burger);
+  };
   return (
     <div>
-      <Navbar />
-      <Sidebar />
-      {props.children}
+      <Sidebar active={burger} />
+      <div
+        className={`${styles.container} ${!burger ? styles.containerFull : ""}`}
+      >
+        <Navbar sidebarActiveHandle={sidebarActiveHandle} />
+
+        <div className={styles.contents}>{props.children}</div>
+      </div>
     </div>
   );
 }
--- client/src/components/Layout/Sidebar/Sidebar.js
+++ client/src/components/Layout/Sidebar/Sidebar.js
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useContext } from "react";
+import styles from "./Sidebar.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import UsersIcon from "../../../assets/icons/UsersIcon";
+import AppContext from "../../../context/AppContext";
+import http from "../../../utils/axios-instance";
+import SettingIcon from "../../../assets/icons/SettingIcon";
 
-function Sidebar() {
-  return <div>Sidebar</div>;
+function Sidebar(props) {
+  const exit = !props.active;
+  const ctx = useContext(AppContext);
+  const { user } = useContext(AppContext);
+  const navigate = useNavigate();
+  const logoutHandle = () => {
+    localStorage.clear();
+    ctx.onReset();
+    http({
+      headers: {
+        authorization: "",
+      },
+    });
+    navigate("/");
+  };
+  return (
+    <div className={`${styles.sidebar} ${exit ? styles.exit : ""}`}>
+      <Link to={"/"} className={styles.headerLink}>
+        <h1 className={`h1 ${styles.sidebarTitle}`}>InterEX Uz</h1>
+      </Link>
+      <div className={styles.mainMenu}>
+        <p className={`subtitle ${styles.subtitle}`}>MAIN MENU</p>
+        <Link to={"/users"} className={styles.sidebarLink}>
+          <UsersIcon classname={styles.sidebarLinkSvg} />
+          <p className="h6">Users</p>
+        </Link>
+        {user.userRole === "ADMIN" && (
+          <Link to={"/packages"} className={styles.sidebarLink}>
+            <UsersIcon classname={styles.sidebarLinkSvg} />
+            <p className="h6">Packages</p>
+          </Link>
+        )}
+        {user.userRole === "STORE_OWNER" ||
+          (user.userRole === "ADMIN" && (
+            <Link to={"/orders"} className={styles.sidebarLink}>
+              <UsersIcon classname={styles.sidebarLinkSvg} />
+              <p className="h6">Orders</p>
+            </Link>
+          ))}
+        <Link className={styles.sidebarLink}>
+          <UsersIcon classname={styles.sidebarLinkSvg} />
+          <p className="h6">Recruitment</p>
+        </Link>
+        <Link className={styles.sidebarLink}>
+          <UsersIcon classname={styles.sidebarLinkSvg} />
+          <p className="h6">Schedule</p>
+        </Link>
+        <Link className={styles.sidebarLink}>
+          <UsersIcon classname={styles.sidebarLinkSvg} />
+          <p className="h6">Department</p>
+        </Link>
+      </div>
+      <div className={styles.other}>
+        <p className={`subtitle ${styles.subtitle}`}>OTHER</p>
+        <Link className={styles.sidebarLink}>
+          <UsersIcon classname={styles.sidebarLinkSvg} />
+          <p className="h6">Support</p>
+        </Link>
+        <Link className={styles.sidebarLink}>
+          <SettingIcon classname={styles.sidebarLinkSvg} />
+          <p className="h6">Settings</p>
+        </Link>
+        <Link onClick={logoutHandle} className={styles.sidebarLink}>
+          <UsersIcon classname={styles.sidebarLinkSvg} />
+          <p className="h6">Log Out</p>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
