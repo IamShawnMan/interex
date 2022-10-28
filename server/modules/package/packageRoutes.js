36a541bac6c03ae2f6cd2482b445df431432f09e
Abdumalik <e.a1994@mail.ru>
2022-10-28 16:44:00 +0500

    edited packageRouter.js

--- server/modules/package/packageRoutes.js
+++ server/modules/package/packageRoutes.js
@@ -2,10 +2,6 @@ const packageControllers = require("./packageControllers")
 const roleMiddleware = require("../../core/middlewares/roleMiddleware")
 const router = require("express").Router()
 
-router
-    .route("/myorders")
-    .get(roleMiddleware(["STORE_OWNER"]), packageControllers.getMyOrders)
-
     router
     .route("/")
     .get(roleMiddleware(["SUPER_ADMIN", "ADMIN"]), packageControllers.getAllPackages)
