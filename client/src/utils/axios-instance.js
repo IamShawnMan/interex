c1831feb2ed5e83ab27090e60a3fdfe12cdc426d
Anvar <choriyevanvar08@gmai.com>
2022-10-31 11:27:35 +0500

    update

--- client/src/utils/axios-instance.js
+++ client/src/utils/axios-instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://192.168.60.112:8080/api/v1",
+  baseURL: "http://localhost:8080/api/v1",
 });
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
--- server/modules/package/packageRoutes.js
+++ server/modules/package/packageRoutes.js
@@ -3,10 +3,6 @@ const roleMiddleware = require("../../core/middlewares/roleMiddleware")
 const router = require("express").Router()
 
 router
-    .route("/myorders")
-    .get(roleMiddleware(["STORE_OWNER"]), packageControllers.getMyOrders)
-
-    router
     .route("/")
     .get(roleMiddleware(["SUPER_ADMIN", "ADMIN"]), packageControllers.getAllPackages)
 
