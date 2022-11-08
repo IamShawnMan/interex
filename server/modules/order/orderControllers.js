30fc64d9220384ed842c6c6f45b62f0f8353aeee
Anvar <choriyevanvar08@gmai.com>
2022-11-08 10:22:52 +0500

    update

--- server/modules/order/orderControllers.js
+++ server/modules/order/orderControllers.js
@@ -339,14 +339,15 @@ exports.getDeliveredOrders = catchAsync(async (req, res, next) => {
 		message: "Yetkazib berilgan buyurtmalar",
 		error: null, 
 		data: {
-			deliveredOrders
+			...deliveredOrders
 		}
 	})
 })
 
 exports.changeStatusDeliveredOrders = catchAsync(async (req, res, next) => {
 	const {id} = req.params
-	const {orderStatus} = req.body
+	const {regionId} = req.user
+	const {orderStatus, note} = req.body
 	const postOrdersById = await OrderModel.findByPk(id, {
 		where: {
 			regionId: {
@@ -358,14 +359,16 @@ exports.changeStatusDeliveredOrders = catchAsync(async (req, res, next) => {
 	const postOrderStatusChange = postOrderStatuses.find(e => e === orderStatus)
 	if(postOrdersById.dataValues.orderStatus === "DELIVERED" 
 	|| postOrdersById.dataValues.orderStatus === "PENDING") {
-		await postOrdersById.update({orderStatus: postOrderStatusChange})
+		await postOrdersById.update({orderStatus: postOrderStatusChange, note})
 	}
 
 	res.status(203).json({
 		status: "success",
 		message: "Post orderining statusi o'zgardi",
 		error: null,
-		data: null
+		data: {
+			note
+		}
 	})
 })
 
