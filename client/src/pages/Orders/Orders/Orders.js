ac5f00b33b9c04e39c58443fb301435efa4fbfdd
Isroil Omonjonov <omonjonovisroil65@gmail.com>
2022-11-10 04:59:14 +0000

    Update client/src/pages/Orders/Orders/Orders.js

--- client/src/pages/Orders/Orders/Orders.js
+++ client/src/pages/Orders/Orders/Orders.js
@@ -53,7 +53,6 @@ function Orders() {
     setPrice(res.data);
   };
   const getAllOrders = async (data) => {
-    console.log(data);
     setValue(data?.data?.content);
     setPagination(data?.data?.pagination);
     setOrdersIdArr(data?.data?.ordersArrInPost);
