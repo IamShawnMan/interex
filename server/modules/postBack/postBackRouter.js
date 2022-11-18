6a80a401b24d16a515deaf68fad9eb2214baee16
Anvar <choriyevanvar08@gmai.com>
2022-11-18 18:16:06 +0500

    update

--- server/modules/postBack/postBackRouter.js
+++ server/modules/postBack/postBackRouter.js
@@ -5,8 +5,9 @@ const postBackController = require("./postBackController");
 module.exports = router
   .get("/rejected/orders", postBackController.rejectedOrdersBeforeSend)
   .get("/rejected/coming", postBackController.getTodaysRejectedPost)
+  .get("/rejected/count", postBackController.countRejectedOrders)
   .post("/new/rejected", postBackController.createPostForAllRejectedOrders)
   .put("/:id/send/rejected", postBackController.sendRejectedPost)
   .get("/rejectedposts", postBackController.getAllRejectedPosts)
   .get("/rejectedposts/:id", postBackController.getAllRejectedOrdersInPost)
-  .put("/new/receiverejectedpost", postBackController.receiveRejectedOrders);
+  .put("/new/receiverejectedpost", postBackController.receiveRejectedOrders)
