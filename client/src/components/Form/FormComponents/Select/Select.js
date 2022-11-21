539798b097f364f5cd6e0a52ae3b8b6077955e3e
Abdumalik <e.a1994@mail.ru>
2022-11-21 10:07:12 +0500

    uzbek_language

--- client/src/components/Form/FormComponents/Select/Select.js
+++ client/src/components/Form/FormComponents/Select/Select.js
@@ -3,50 +3,50 @@ import styles from "./Select.module.css";
 import AttentionError from "../../../../assets/icons/AttentionError";
 
 function Select({
-  data,
-  children,
-  onChange,
-  error,
-  register,
-  id,
-  placeholder,
+	data,
+	children,
+	onChange,
+	error,
+	register,
+	id,
+	placeholder,
 }) {
-  return (
-    <div className={styles.formControl}>
-      <div className={styles.selectContainer}>
-        <div className={styles.selectControl}>
-          {children && (
-            <label className={styles.label} htmlFor={id ? id : ""}>
-              {children}
-            </label>
-          )}
-          <select
-            className={`${styles.select} ${error ? styles.error : ""}`}
-            {...(register ? register() : "")}
-            onChange={onChange}
-          >
-            <option className={styles.option} value={""}>
-              {placeholder}
-            </option>
-            {data ? (
-              data.map((e) => (
-                <option className={styles.option} key={e.id} value={e.id}>
-                  {e.name}
-                </option>
-              ))
-            ) : (
-              <option className={styles.option} value={""}>
-                Ma'lumotlar yo'q
-              </option>
-            )}
-          </select>
-        </div>
-      </div>
-      <p className={error ? styles.errorText : styles.errorTextNone}>
-        {error ? error : "default Error"}
-      </p>
-    </div>
-  );
+	return (
+		<div className={styles.formControl}>
+			<div className={styles.selectContainer}>
+				<div className={styles.selectControl}>
+					{children && (
+						<label className={styles.label} htmlFor={id ? id : ""}>
+							{children}
+						</label>
+					)}
+					<select
+						className={`${styles.select} ${error ? styles.error : ""}`}
+						{...(register ? register() : "")}
+						onChange={onChange}
+					>
+						<option className={styles.option} value={""}>
+							{placeholder}
+						</option>
+						{data ? (
+							data.map((e) => (
+								<option className={styles.option} key={e.id} value={e.name ? e.id : e.en}>
+									{e.name ? e.name : e.uz}
+								</option>
+							))
+						) : (
+							<option className={styles.option} value={""}>
+								Ma'lumotlar yo'q
+							</option>
+						)}
+					</select>
+				</div>
+			</div>
+			<p className={error ? styles.errorText : styles.errorTextNone}>
+				{error ? error : "default Error"}
+			</p>
+		</div>
+	);
 }
 
 export default Select;
