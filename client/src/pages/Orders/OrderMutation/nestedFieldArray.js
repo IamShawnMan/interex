6bd25480085400f321a7f48f74138a7778477605
Isroil Omonjonov <omonjonovisroil65@gmail.com>
2022-11-22 14:18:31 +0500

    order mutation style changed

--- client/src/pages/Orders/OrderMutation/nestedFieldArray.js
+++ client/src/pages/Orders/OrderMutation/nestedFieldArray.js
@@ -81,7 +81,7 @@ function OrderItems(props) {
         className={styles.btnIconTextContainer}
         onClick={() => append({ productName: "", quantity: "", price: "" })}
       >
-        <Button type="button" name="iconText" iconName="plus">
+        <Button type="button" name="icon" iconName="plus">
           Mahsulot
         </Button>
       </div>
