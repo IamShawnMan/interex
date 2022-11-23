85bb470639db93be463baee04d9c323056777a5f
Isroil Omonjonov <omonjonovisroil65@gmail.com>
2022-11-23 14:20:27 +0500

    order component add createdAt col

--- client/src/components/Layout/Sidebar/Sidebar.js
+++ client/src/components/Layout/Sidebar/Sidebar.js
@@ -93,6 +93,46 @@ function Sidebar(props) {
               </p>
             </Link>
           )}
+          {user.userRole === "COURIER" && (
+          <>
+            <Link
+              to={"/new-post"}
+              className={`${styles.sidebarLink} ${
+                open ? styles.sidebarLinkOpen : styles.sidebarLinkClouse
+              }`}
+            >
+              <DashboardIcon classname={styles.sidebarLinkSvg} />
+              <p className={`h6 ${!open ? styles.linkP_hidden : styles.linkP}`}>
+                Pochta
+              </p>
+            </Link>
+
+            <Link
+              to="/postback/rejected/orders"
+              className={`${styles.sidebarLink} ${
+                open ? styles.sidebarLinkOpen : styles.sidebarLinkClouse
+              }`}
+            >
+              <UsersIcon classname={styles.sidebarLinkSvg} />
+              <p className={`h6 ${!open ? styles.linkP_hidden : styles.linkP}`}>
+                Pochta Yaratish
+              </p>
+            </Link>
+
+            <Link
+              to="/postback"
+              className={`${styles.sidebarLink} ${
+                open ? styles.sidebarLinkOpen : styles.sidebarLinkClouse
+              }`}
+            >
+              <UsersIcon classname={styles.sidebarLinkSvg} />
+              <p className={`h6 ${!open ? styles.linkP_hidden : styles.linkP}`}>
+                Mening pochtalarim
+              </p>
+            </Link>
+          </>
+        )}
+
           {user.userRole === "ADMIN" && (
             <Link
               to={"/packages"}
@@ -119,6 +159,7 @@ function Sidebar(props) {
               </p>
             </Link>
           )}
+          
         </div>
         <div className={`${styles.other} ${styles.linksContainer}`}>
           <p
