ecd05a9c9d4da15e92f8cda7e406fdf0c5a75757
Anvar <choriyevanvar08@gmai.com>
2022-11-28 11:36:13 +0500

    role middlewares for posts

--- client/src/pages/Orders/Orders/Orders.js
+++ client/src/pages/Orders/Orders/Orders.js
@@ -499,20 +499,13 @@ function Orders() {
       //   }
       //   `,
       // });
-<<<<<<< HEAD
-      const res = await http(`${url?url:""}?${page?`page=${page}`:""}${size?`&size=${size}`:""}${search?`&search=${search}`:""}${orderStatus?`&orderStatus=${orderStatus}`:""}${!isStoreOwner?storeOwnerId?`&storeOwnerId=${storeOwnerId}`:"":""}${regionId ? `&regionId=${regionId}` : ""}${districtId?`&districtId=${districtId}`:""}${dateCreatedAt ? `&createdAt[eq]=${dateCreatedAt.toISOString()}`:""}`)
-=======
       let res
       // console.log(url);
       if((url === "/orders" || url === "/orders/delivered" ||   url === "/orders/myorders")){
-        console.log("iffffffffffffffff");
       res = await http(`${url?url:""}?${page?`page=${page}`:""}${size?`&size=${size}`:""}${search?`&search=${search}`:""}${orderStatus?`&orderStatus=${orderStatus}`:""}${!isStoreOwner?storeOwnerId?`&storeOwnerId=${storeOwnerId}`:"":""}${regionId ? `&regionId=${regionId}` : ""}${districtId?`&districtId=${districtId}`:""}${dateCreatedAt ? `&createdAt[eq]=${dateCreatedAt.toISOString()}`:""}`)
       }else{
-        console.log("elseeeeeeeeeeeeeeee");
         res = await http(`${url?url:""}?${search?`search=${search}`:""}`)
       }
-      console.log(res.data);
->>>>>>> b1c3d04345b93bb8a1821f05b016e60ee66a0a52
 
       getAllOrders(res.data);
     } catch (error) {
