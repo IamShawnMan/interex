d843cad098075a6c5d8a3ed53b0eb91099876167
Shodiyor Ergashev <mybusinesskebap@gmail.com>
2022-12-24 05:56:50 +0500

    created at changed to updated at

--- server/core/utils/QueryBuilder.js
+++ server/core/utils/QueryBuilder.js
@@ -1,8 +1,8 @@
-const {Op} = require("sequelize");
-const dayjs = require("dayjs")
+const { Op } = require("sequelize");
+const dayjs = require("dayjs");
 
 const excludeParams = ["page", "size", "fields", "search", "sort"];
-const operators = ["gte", "gt", "lt", "lte", "in", "eq"]
+const operators = ["gte", "gt", "lt", "lte", "in", "eq"];
 
 class QueryBuilder {
 	constructor(queryParams) {
@@ -10,48 +10,53 @@ class QueryBuilder {
 		this.queryOptions = {};
 	}
 
-    filter(){
-     const filterFields = {...this.queryParams}
-     excludeParams.forEach(p=>delete filterFields[p])
-     const filterObject = {};
-     Object.keys(filterFields).forEach(k=>{
-        const filterItem = filterFields[k]
-        if(typeof filterItem === "object"){
-            Object.keys(filterItem).forEach((ik)=>{
-               if(Object.keys(filterFields[k]).length>1){
-               if(filterObject[k]){
-                filterObject[k] = {...{[Op[ik]]: filterItem[ik]},...filterObject[k]}
-               }else{
-                 filterObject[k]={[Op[ik]]: filterItem[ik]}
-               }
-                return
-            }
-            if(Object.keys(filterFields[k])[0]==="in"){
-                filterObject[k] = {[Op[ik]]: filterItem[ik].split(",")}
-               return
-             }
-             if(Object.keys(filterFields[k])[0]==="eq"){
-                filterObject[k] = {
-                    [Op["gte"]]: filterItem[ik],
-                    [Op["lte"]]: dayjs(`${filterItem[ik]}`).endOf("day").format("YYYY-MM-DDTHH:mm:ss.SSS[Z]") 
-                }
-               return
-             }
-                if(operators.includes(ik)){
-                    filterObject[k] = {[Op[ik]]: filterItem[ik]}
-                }
-        })
-        }else{
-            filterObject[k] = {[Op.eq]: filterItem}
-        }
-     })
-     if(this.queryOptions.where){
-        this.queryOptions.where =  {...filterObject, ...this.queryOptions.where}; 
-     }else{
-        this.queryOptions.where = filterObject;
-     }
-     return this;
-    }
+	filter() {
+		const filterFields = { ...this.queryParams };
+		excludeParams.forEach((p) => delete filterFields[p]);
+		const filterObject = {};
+		Object.keys(filterFields).forEach((k) => {
+			const filterItem = filterFields[k];
+			if (typeof filterItem === "object") {
+				Object.keys(filterItem).forEach((ik) => {
+					if (Object.keys(filterFields[k]).length > 1) {
+						if (filterObject[k]) {
+							filterObject[k] = {
+								...{ [Op[ik]]: filterItem[ik] },
+								...filterObject[k],
+							};
+						} else {
+							filterObject[k] = { [Op[ik]]: filterItem[ik] };
+						}
+						return;
+					}
+					if (Object.keys(filterFields[k])[0] === "in") {
+						filterObject[k] = { [Op[ik]]: filterItem[ik].split(",") };
+						return;
+					}
+					if (Object.keys(filterFields[k])[0] === "eq") {
+						filterObject[k] = {
+							[Op["gte"]]: filterItem[ik],
+							[Op["lte"]]: dayjs(`${filterItem[ik]}`)
+								.endOf("day")
+								.format("YYYY-MM-DDTHH:mm:ss.SSS[Z]"),
+						};
+						return;
+					}
+					if (operators.includes(ik)) {
+						filterObject[k] = { [Op[ik]]: filterItem[ik] };
+					}
+				});
+			} else {
+				filterObject[k] = { [Op.eq]: filterItem };
+			}
+		});
+		if (this.queryOptions.where) {
+			this.queryOptions.where = { ...filterObject, ...this.queryOptions.where };
+		} else {
+			this.queryOptions.where = filterObject;
+		}
+		return this;
+	}
 
 	limitFields() {
 		if (this.queryParams.fields?.length > 0) {
@@ -66,8 +71,8 @@ class QueryBuilder {
 	}
 
 	paginate() {
-		const page = this.queryParams.page ||= 1
-		const limit = this.queryParams.size ||= 100
+		const page = (this.queryParams.page ||= 1);
+		const limit = (this.queryParams.size ||= 500);
 		this.queryOptions.limit = limit;
 		this.queryOptions.offset = (page - 1) * limit;
 		return this;
