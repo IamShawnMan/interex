ee4380733cc20b1101d5c402f566f261e0ac8284
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2023-02-25 12:52:52 +0500

    front update

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import Sidebar from "./Sidebar/Sidebar";
 import Navbar from "./Navbar/Navbar";
-import ArrrowForBtn from "../../assets/icons/ArrowForBtn";
+// import ArrrowForBtn from "../../assets/icons/ArrowForBtn";
 import styles from "./Layout.module.css";
 import styles1 from "./Navbar/Navbar.module.css";
 
 import { useNavigate } from "react-router-dom";
-import {ReactComponent as BackIcon} from "../../assets/icons/backIcon.svg";
+import { ReactComponent as BackIcon } from "../../assets/icons/backIcon.svg";
 
 function Layout(props) {
   const [arrowChange, setArrowChange] = useState(true);
@@ -27,23 +27,32 @@ function Layout(props) {
         } ${!arrowChange ? styles.hiddenContainer : styles.containerTablet}`}
       >
         <Navbar
-        info={props.info}
+          info={props.info}
           sidebarActiveHandle={sidebarActiveHandle}
           setSearch={props.setSearch}
         />
-        <div className={styles.layoutBox}>
-          <div style={{display: 'flex',alignItems: 'center',justifyContent: 'space-between'}}>
-          <div className={styles.pathRoad}>
-            <h1 className={`h2 ${styles.pageName}`}>{props.pageName}</h1>
-          </div>
-          <button
-			className={styles1.backIcon}
+        <div className={styles.layoutBox}
+         style={{ paddingTop: "9rem" }}
+         >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <div className={styles.pathRoad}>
+              {/* <h1 className={`h2 ${styles.pageName}`}>{props.pageName}</h1> */}
+            </div>
+            <button
+              className={styles1.backIcon}
               onClick={() => {
                 navigate(-1);
               }}
             >
-              {<BackIcon/>}
-            </button></div>
+              {<BackIcon />}
+            </button>
+          </div>
           <div className={styles.contents}>{props.children}</div>
         </div>
       </div>
